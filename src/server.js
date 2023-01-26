15ac7ca585722911a253dc01572e131de2628c1d
r1tter <marcelosori@hotmail.com>
2023-01-26 12:04:03 -0300

    Database basics

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http';
+import { Database } from './database.js';
 import { json } from './middlewares/jason.js';
 
-const users = [];
+const database = new Database()
 
 const server = http.createServer(async(req, res) => {
 
@@ -10,18 +11,21 @@ const server = http.createServer(async(req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-    .end(JSON.stringify(users))
+    const users = database.select('users');
+
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
+    const user ={
       id: 1,
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
